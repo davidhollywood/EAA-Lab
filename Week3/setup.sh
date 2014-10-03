cbd2a58469991b0bdcf6b22c8e1c5a009cd628b8
davidhollywood <davidhollywood123@hotmail.com>
2014-10-03 11:00:52 +0100

    folders being added

---
+++ Week3/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#: Title : Lab week 2 script
+#: Date : 03/10/14
+#: Author : David Hollywood
+#: Version : ?
+#: Description : Write a script that creates two subdirectories, bin and scripts 
+#: Options : ?
+
+cd $HOME/EAA-Lab/week3
+mkdir folder1
+cd folder1
+mkdir folder2
+mkdir bin
+
+
+
+

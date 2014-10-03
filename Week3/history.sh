5f7104acae47b747027381b157a3086ad3e66989
davidhollywood <davidhollywood123@hotmail.com>
2014-10-03 11:35:14 +0100

    updated history file for week 3

---
+++ Week3/history.sh
@@ -0,0 +1,185 @@
+    1  ls -ltr
+    2  sudo gedit /etc/cntlm.conf 
+    3  bg
+    4  cd
+    5  find . -name *.crt -print 2>/dev/null
+    6  cd Downloads/
+    7  ls
+    8  vi eduroam-linux-IoTT_Linux.sh 
+    9  cd
+   10  sudo gedit /etc/cntlm.conf 
+   11  sudo gedit /etc/cntlm.conf 
+   12  bg
+   13  cntlm -M
+   14  cntlm -H
+   15  cntlm -H
+   16  cntlm -H
+   17  cntlm -H
+   18  sudo service cntlm restart
+   19  cntlm -H
+   20  cntlm -H cntlm.conf
+   21  cntlm cntlm.conf -H
+   22  cntlm -H
+   23  cntlm -H www.sun.com
+   24  cntlm -H
+   25  cntlm -H
+   26  ls /etc
+   27  sudo mv /etc/cntlm.conf /etc/cntlm.conf.old
+   28  cntlm -H
+   29  ls
+   30  sudo apt-get remove cntlm
+   31  sudo apt-get install cntlm
+   32  sudo apt-get remove cntlm
+   33  sudo apt-get install cntlm
+   34  sudo service start cntlm
+   35  sudo apt-get install cntlm
+   36  sudo service start 
+   37  sudo service cntlm start
+   38  sudo service cntlm start -v
+   39  sudo apt-get remove cntlm
+   40  sudo apt-get install cntlm
+   41  sudo apt-get remove cntlm
+   42  sudo apt-get install cntlm
+   43  sudo apt-get remove cntlm
+   44  find / -name cntlm.conf -print
+   45  find / -name cntlm.conf -print 2>/dev/nul
+   46  find / -name cntlm.conf -print 2>/dev/null
+   47  sudo apt-get remove cntlm
+   48  sudo apt-get install cntlm
+   49  sudo apt-get install cntlm
+   50  ls /etc/cntlm.conf
+   51  sudo apt-get purge cntlm
+   52  sudo apt-get install cntlm
+   53  find / -name cntlm.conf -print 2>/dev/null
+   54  cntlm -H
+   55  sudo vi /etc/cntlm.conf
+   56  cntlm -H
+   57  fg
+   58  sudo service cntlm restart
+   59  vi eduroam-linux-IoTT_Linux.sh 
+   60  sudo vi /etc/cntlm.conf
+   61  cntlm -M
+   62  cntlm -H -c /etc/cntlm.conf
+   63  cntlm -c /etc/cntlm.conf -H
+   64  cntlm -H
+   65  cntlm -c /etc/cntlm.conf -H
+   66  cntlm -c /etc/cntlm.conf 
+   67  cntlm -H -u jburns@stf
+   68  sudo vi /etc/cntlm.conf
+   69  sudo service cntlm restart
+   70  sudo vi /etc/cntlm.conf
+   71  sudo service cntlm restart
+   72  sudo service cntlm restart
+   73  sudo service cntlm restart
+   74  ls -ltr
+   75  rm *.cer
+   76  ls
+   77  chef
+   78  ls /opt
+   79  /opt/chef/bin/chef-solo
+   80  sudo shutdown -r now
+   81  ls -ltr
+   82  vi .profile 
+   83  export GIT_SSL_NO_VERIFY=1
+   84  export http_proxy=http://localhost:3128
+   85  sudo -E apt-get install git
+   86  git config --global user.email "davidhollywood123@hotmail.com"
+   87  git config --global user.name "<your username>"
+   88  git config --global user.name "davidhollywood"
+   89  git clone https://github.com/davidhollywood/EAA.week2.git
+   90  gedit
+   91  cd /
+   92  pwd
+   93  pwd
+   94  cd EAA.week2
+   95  cd /EAA.week2
+   96  cd home
+   97  cd EAA.week2
+   98  pwd
+   99  ls
+  100  ls -f
+  101  cd..
+  102  cd EAA.week2
+  103  cd EAA.week2
+  104  cd
+  105  ls
+  106  cd EAA.week2
+  107  ls
+  108  ls
+  109  cd ..
+  110  mkdir EAA-Lab
+  111  cd EAA-Lab
+  112  git clone https://github.com/davidhollywood/EAA-Lab.git
+  113  ls
+  114  cd week 3
+  115  cd Week 3
+  116  ls
+  117  cd Week3
+  118  gedit script.sh
+  119  gedit setup.sh
+  120  gedit hello.md
+  121  git commit -a
+  122  cd EAA-Lab
+  123  cd /
+  124  ls
+  125  cd home
+  126  ls
+  127  cd EAA-Lab
+  128  cd/
+  129  cd /
+  130  cd EAA-Lab
+  131  ls
+  132  cd home
+  133  ls
+  134  ls 
+  135  pwd
+  136  ls
+  137  cd /mint
+  138  cd mint
+  139  cd EAA-Lab
+  140  git add -A
+  141  export GIT_SSL_NO_VERIFY=1
+  142  export http_proxy=http://localhost:3128
+  143  git config --global user.email "davidhollywood123@hotmail.com"
+  144  git config --global user.name "davidhollywood"
+  145  git clone https://github.com/davidhollywood/EAA-Lab.git
+  146  git add -A
+  147  git commit -a
+  148  git clone https://github.com/davidhollywood/EAA-Lab.git
+  149  ls
+  150  git add -A
+  151  cd EAA-Lab
+  152  git add -A
+  153  git commit -a
+  154  git add -A
+  155  git commit -a
+  156  git push origin master
+  157  cd ..
+  158  cd ..
+  159  ls
+  160  pwd
+  161  mkdir demo
+  162  cd demo
+  163  mkdir week3
+  164  cd week3
+  165  gedit
+  166  cd ..
+  167  export GIT_SSL_NO_VERIFY=1
+  168  export http_proxy=http://localhost:3128
+  169  git config --global user.email "davidhollywood123@hotmail.com"
+  170  git config --global user.name "davidhollywood"
+  171  git clone https://github.com/davidhollywood/demo.git
+  172  git add -A
+  173  cd week3
+  174  git add -A
+  175  git commit -a
+  176  cd..
+  177  cd ..
+  178  git commit -a
+  179  git add -A
+  180  git commit -a
+  181  push origin master
+  182  git push origin master
+  183  cd ..
+  184  cd ..
+  185  history > history.sh

566c295d103bae63ec573a30e0c982fbd2130f34
davidhollywood <davidhollywood123@hotmail.com>
2014-10-10 11:58:20 +0100

    commiting changes

---
+++ Week4/diary.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+#: Title : diary script
+#: Date : 10/10/2014
+#: Author : david hollywood
+#: Version : 1.0
+#: Description : adding entries to diary 
+#: Options : 
+
+printf "Add an entry to your diary now:"
+read input_variable
+echo $input_variable1 >> diary.txt
